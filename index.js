9430225d2f963f2aaf0c858a8b56bfab5c87f1ce
RianMaciel <147260277+RianMaciel@users.noreply.github.com>
2023-12-02 15:09:28 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+  let num1 = Number(prompt("Digite o primeiro número: "))
+  let num2 = Number(prompt("Digite o segundo número: "))
+  let num3 = Number(prompt("Digite o terceiro número: "))
+  let num4 = Number(prompt("Digite o quarto número: ")) 
+  
+  soma= num1+num2+num3+num4
+
+media = soma/4
+
+ alert('Resultado da soma é '  +soma)
+ alert('Resultado da media é ' +media)
+
+let quantidade = Number(prompt('Digite a quantidade de numeros'))
+  soma = 0
+var i = 0 
+
+while(i<=quantidade){
+  num = Number(prompt('Digite um numero'))
+  soma += num
+  i++
+}
+alert( 'Resultado da soma é ' + soma)
+
+num1 = Number(prompt("Digite o primeiro número  "))
+num2 = Number(prompt("Digite o segundo número  "))
+var soma = 0
+
+ if (num %3 == 1){
+  soma +=num
+ }
+
+ alert('Resultado da soma é ' + soma)
+  
+
+  
